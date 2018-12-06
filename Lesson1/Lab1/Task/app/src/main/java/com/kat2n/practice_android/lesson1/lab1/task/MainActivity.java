c3f8ff36c653435b7e77ac73ed8e86080141b64c
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-05 23:01:21 -0800

    Lesson1 Lab1 Task

---
+++ Lesson1/Lab1/Task/app/src/main/java/com/kat2n/practice_android/lesson1/lab1/task/MainActivity.java
@@ -0,0 +1,19 @@
+package com.kat2n.practice_android.lesson1.lab1.task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+  private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    Log.d(LOG_TAG, "Hello World");
+
+  }
+}
