5e98216307bd1697f0f24902a1d123e247b70074
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-06 01:24:52 -0800

    copy Lesson1/Lab2/Task to Lesson1/Lab3/Task

---
+++ Lesson1/Lab3/Task/app/src/test/java/com/kat2n/practice_android/lesson1/lab2/task/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.kat2n.practice_android.lesson1.lab2.task;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+  @Test
+  public void addition_isCorrect() {
+    assertEquals(4, 2 + 2);
+  }
+}
