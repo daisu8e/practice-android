0f9f7f5f49a14d5021f794f2409c453048ac95af
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-06 03:05:33 -0800

    Lesson2 Lab1 Homework

--- Lesson2/Lab1/Homework/app/src/main/java/com/kat2n/practice_android/lesson2/lab1/homework/MainActivity.java
+++ Lesson2/Lab1/Homework/app/src/main/java/com/kat2n/practice_android/lesson2/lab1/homework/MainActivity.java
@@ -1,14 +1,16 @@
 package com.kat2n.practice_android.lesson2.lab1.homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-  private int mCount = 0;
+  public static final String EXTRA_MESSAGE = "com.kat2n.practice_android.lesson2.lab1.homework.extra.MESSAGE";
+
+  private Integer mCount = 0;
   private TextView mShowCount;
 
   @Override
@@ -18,13 +20,16 @@ public class MainActivity extends AppCompatActivity {
     mShowCount = (TextView) findViewById(R.id.show_count);
   }
 
-  public void showToast(View view) {
-    Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
-    toast.show();
+  public void launchSecondActivity(View view) {
+    Intent intent = new Intent(this, SecondActivity.class);
+    String message = mCount.toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivity(intent);
   }
 
   public void countUp(View view) {
     ++mCount;
     if (mShowCount != null) mShowCount.setText(Integer.toString(mCount));
   }
+
 }
