0bf9f7bbaacabf18a0e0905d216020325e5c0b00
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-07 22:11:57 -0800

    copy Lesson2/Lab2/Challenge to Lesson2/Lab3/Challenge

---
+++ Lesson2/Lab3/Challenge/app/src/main/java/com/kat2n/practice_android/lesson2/lab2/challenge/MainActivity.java
@@ -0,0 +1,59 @@
+package com.kat2n.practice_android.lesson2.lab2.challenge;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+  public static final int REQUEST_CODE = 1;
+
+  private List<String> shoppingList = new ArrayList<>();
+  private List<TextView> itemFields = new ArrayList<>();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    itemFields.add((TextView)findViewById(R.id.itemField0));
+    itemFields.add((TextView)findViewById(R.id.itemField1));
+    itemFields.add((TextView)findViewById(R.id.itemField2));
+    itemFields.add((TextView)findViewById(R.id.itemField3));
+    itemFields.add((TextView)findViewById(R.id.itemField4));
+    itemFields.add((TextView)findViewById(R.id.itemField5));
+    itemFields.add((TextView)findViewById(R.id.itemField6));
+    itemFields.add((TextView)findViewById(R.id.itemField7));
+    itemFields.add((TextView)findViewById(R.id.itemField8));
+    itemFields.add((TextView)findViewById(R.id.itemField9));
+  }
+
+  public void touchAddItemButton(View view) {
+    Intent intent = new Intent(this, SecondActivity.class);
+    startActivityForResult(intent, REQUEST_CODE);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == REQUEST_CODE) {
+      if (resultCode == RESULT_OK) {
+        String item = data.getStringExtra(SecondActivity.EXTRA_ITEM);
+        shoppingList.add(item);
+        render();
+      }
+    }
+  }
+
+  private void render() {
+    for (int i = 0; i < itemFields.size(); i++) {
+      if (i < shoppingList.size()) itemFields.get(i).setText(shoppingList.get(i));
+      else itemFields.get(i).setText("");
+    }
+  }
+}
---
+++ Lesson2/Lab3/Challenge/app/src/main/java/com/kat2n/practice_android/lesson2/lab2/challenge/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.kat2n.practice_android.lesson2.lab2.challenge;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+  public static final String EXTRA_ITEM = "com.kat2n.practice_android.lesson2.lab2.challenge.extra.ITEM";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_second);
+  }
+
+  public void touchItemButton(View view) {
+    String item = "";
+    switch (view.getId()) {
+      case R.id.riceButton:     item = "Rice";      break;
+      case R.id.potatoesButton: item = "Potatoes";  break;
+      case R.id.carrotsButton:  item = "Carrots";   break;
+      case R.id.onionsButton:   item = "Onions";    break;
+      case R.id.curryButton:    item = "curry";     break;
+    }
+    Intent intent = new Intent();
+    intent.putExtra(EXTRA_ITEM, item);
+    setResult(RESULT_OK, intent);
+    finish();
+  }
+}
