273071bd53c24dd4f02ebd77c8380c0ec6988aa2
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-07 22:29:48 -0800

    Lesson2 Lab3 Challenge

--- Lesson2/Lab3/Challenge/app/src/main/java/com/kat2n/practice_android/lesson2/lab3/challenge/MainActivity.java
+++ Lesson2/Lab3/Challenge/app/src/main/java/com/kat2n/practice_android/lesson2/lab3/challenge/MainActivity.java
@@ -1,9 +1,12 @@
 package com.kat2n.practice_android.lesson2.lab3.challenge;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
   private List<String> shoppingList = new ArrayList<>();
   private List<TextView> itemFields = new ArrayList<>();
+  private EditText mLocationEditText;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     itemFields.add((TextView)findViewById(R.id.itemField7));
     itemFields.add((TextView)findViewById(R.id.itemField8));
     itemFields.add((TextView)findViewById(R.id.itemField9));
+    mLocationEditText = findViewById(R.id.location_edittext);
   }
 
   public void touchAddItemButton(View view) {
@@ -56,4 +61,16 @@ public class MainActivity extends AppCompatActivity {
       else itemFields.get(i).setText("");
     }
   }
+
+  public void openLocation(View view) {
+    String loc = mLocationEditText.getText().toString();
+    Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+    Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+    if (intent.resolveActivity(getPackageManager()) != null) {
+      startActivity(intent);
+    } else {
+      Log.d("ImplicitIntents", "Can't handle this intent!");
+    }
+  }
+
 }
