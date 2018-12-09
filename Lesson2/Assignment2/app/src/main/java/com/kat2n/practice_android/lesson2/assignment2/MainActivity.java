88d5ddc77f0d6c93df3b41b79fa0ec939d3c01bf
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-09 00:23:47 -0800

    Lesson2/Assignment2

--- Lesson2/Assignment2/app/src/main/java/com/kat2n/practice_android/lesson2/assignment2/MainActivity.java
+++ Lesson2/Assignment2/app/src/main/java/com/kat2n/practice_android/lesson2/assignment2/MainActivity.java
@@ -6,39 +6,61 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+  private static final List<String> MOVIE_ID_LIST = new ArrayList<>();
+  static {
+    MOVIE_ID_LIST.add("catch_me_if_you_can");
+    MOVIE_ID_LIST.add("fight_club");
+    MOVIE_ID_LIST.add("forrest_gump");
+    MOVIE_ID_LIST.add("good_will_hunting");
+    MOVIE_ID_LIST.add("pulp_fiction");
+    MOVIE_ID_LIST.add("the_godfather");
+    MOVIE_ID_LIST.add("the_hangover");
+    MOVIE_ID_LIST.add("the_shawshank_redemption");
+    MOVIE_ID_LIST.add("titanic");
+  }
+
   private Spinner spinner;
+  private ImageView imageView;
+  private TextView textView;
+  private String movieId;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    spinner = findViewById(R.id.moviesSpinner);
 
+    spinner = findViewById(R.id.moviesSpinner);
     spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
       @Override
       public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+        movieId = MOVIE_ID_LIST.get(position);
+        render();
       }
       @Override
-      public void onNothingSelected() {
-
+      public void onNothingSelected(AdapterView<?> parent) {
       }
     });
+    imageView = findViewById(R.id.imageView);
+    textView = findViewById(R.id.textView);
+  }
 
-    ImageView imageView = new ImageView(this);
-    imageView.setImageResource(R.drawable.catch_me_if_you_can);
+  private void render() {
+    int imageId = getResources().getIdentifier(movieId, "drawable", getPackageName());
+    imageView.setImageResource(imageId);
 
-    int id = getResources().getIdentifier("the_godfatehr", "raw", getPackageName());
-    String result = "";
-    Scanner in = new Scanner(getResources().openRawResource(R.raw.the_godfather));
-    while (in.hasNext()) {
-      result += in.nextLine();
-    }
-    in.close();
+    int textId = getResources().getIdentifier(movieId, "raw", getPackageName());
+    String text = "";
+    Scanner scanner = new Scanner(getResources().openRawResource(textId));
+    while (scanner.hasNext()) text += scanner.nextLine();
+    scanner.close();
+    textView.setText(text);
   }
 }
