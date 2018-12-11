97b58d3238cb75ce3e71278a2700e24130b736a5
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-11 15:01:23 -0800

    Lesson3 Lab2 Homework

--- Lesson3/Lab2/Homework/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
+++ Lesson3/Lab2/Homework/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
@@ -50,55 +50,97 @@ public class CalculatorTest {
    */
   @Test
   public void addTwoNumbers() {
-    double resultAdd = mCalculator.add(1d, 1d);
-    assertThat(resultAdd, is(equalTo(2d)));
+    double result = mCalculator.add(1d, 1d);
+    assertThat(result, is(equalTo(2d)));
   }
 
   @Test
   public void addTwoNumbersNegative() {
-    double resultAdd = mCalculator.add(-1d, 2d);
-    assertThat(resultAdd, is(equalTo(1d)));
+    double result = mCalculator.add(-1d, 2d);
+    assertThat(result, is(equalTo(1d)));
   }
 
   @Test
   public void addTwoNumbersFloats() {
-    double resultAdd = mCalculator.add(1.111f, 1.111d);
-    assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+    double result = mCalculator.add(1.111f, 1.111d);
+    assertThat(result, is(closeTo(2.222, 0.01)));
   }
 
   @Test
   public void subTwoNumbers() {
-    double resultAdd = mCalculator.sub(1d, 1d);
-    assertThat(resultAdd, is(equalTo(0d)));
+    double result = mCalculator.sub(1d, 1d);
+    assertThat(result, is(equalTo(0d)));
   }
 
   @Test
   public void subWorksWithNegativeResults () {
-    double resultAdd = mCalculator.sub(1d, 2d);
-    assertThat(resultAdd, is(equalTo(-1d)));
+    double result = mCalculator.sub(1d, 2d);
+    assertThat(result, is(equalTo(-1d)));
   }
 
   @Test
   public void mulTwoNumbers() {
-    double resultAdd = mCalculator.mul(2d, 3d);
-    assertThat(resultAdd, is(equalTo(6d)));
+    double result = mCalculator.mul(2d, 3d);
+    assertThat(result, is(equalTo(6d)));
   }
 
   @Test
   public void mulTwoNumbersZero() {
-    double resultAdd = mCalculator.mul(2d, 0d);
-    assertThat(resultAdd, is(equalTo(0d)));
+    double result = mCalculator.mul(2d, 0d);
+    assertThat(result, is(equalTo(0d)));
   }
 
   @Test
   public void divTwoNumbers() {
-    double resultAdd = mCalculator.div(4d, 2d);
-    assertThat(resultAdd, is(equalTo(2d)));
+    double result = mCalculator.div(4d, 2d);
+    assertThat(result, is(equalTo(2d)));
   }
 
   @Test
   public void divTwoNumbersZero() {
-    double resultAdd = mCalculator.div(4d, 0d);
-    assertThat(resultAdd, is(equalTo(Double.POSITIVE_INFINITY)));
+    double result = mCalculator.div(4d, 0d);
+    assertThat(result, is(equalTo(Double.POSITIVE_INFINITY)));
+  }
+
+  @Test
+  public void powTwoNumbers() {
+    double result = mCalculator.pow(2d, 3d);
+    assertThat(result, is(equalTo(8d)));
+  }
+
+  @Test
+  public void powNegativeNumberAndPositiveNumber() {
+    double result = mCalculator.pow(-2d, 3d);
+    assertThat(result, is(equalTo(-8d)));
+  }
+
+  @Test
+  public void powPositiveNumberAndNegativeNumber() {
+    double result = mCalculator.pow(2d, -3d);
+    assertThat(result, is(equalTo(0.125d)));
+  }
+
+  @Test
+  public void powZeroAndPositiveNumber() {
+    double result = mCalculator.pow(0d, 3d);
+    assertThat(result, is(equalTo(0d)));
+  }
+
+  @Test
+  public void powPositiveNumberAndZero() {
+    double result = mCalculator.pow(2d, 0d);
+    assertThat(result, is(equalTo(1d)));
+  }
+
+  @Test
+  public void powZeroAndNegativeOne() {
+    double result = mCalculator.pow(0d, -1d);
+    assertThat(result, is(equalTo(Double.POSITIVE_INFINITY)));
+  }
+
+  @Test
+  public void powNegativeZeroAndNegativeNumber() {
+    double result = mCalculator.pow(-0d, -3d);
+    assertThat(result, is(equalTo(Double.NEGATIVE_INFINITY)));
   }
 }
