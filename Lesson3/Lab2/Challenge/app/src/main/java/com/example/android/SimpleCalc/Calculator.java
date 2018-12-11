3714d9456c60b5c4cd0e5fe02fd5aabe3c8229f7
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-11 14:15:12 -0800

    Lesson3 Lab2 Challenge1

--- Lesson3/Lab2/Challenge/app/src/main/java/com/example/android/SimpleCalc/Calculator.java
+++ Lesson3/Lab2/Challenge/app/src/main/java/com/example/android/SimpleCalc/Calculator.java
@@ -21,34 +21,35 @@ package com.example.android.SimpleCalc;
  */
 public class Calculator {
 
-    // Available operations
-    public enum Operator {ADD, SUB, DIV, MUL}
+  // Available operations
+  public enum Operator {ADD, SUB, DIV, MUL}
 
-    /**
-     * Addition operation
-     */
-    public double add(double firstOperand, double secondOperand) {
-        return firstOperand + secondOperand;
-    }
+  /**
+   * Addition operation
+   */
+  public double add(double firstOperand, double secondOperand) {
+    return firstOperand + secondOperand;
+  }
 
-    /**
-     * Subtract operation
-     */
-    public double sub(double firstOperand, double secondOperand) {
-        return firstOperand - secondOperand;
-    }
+  /**
+   * Subtract operation
+   */
+  public double sub(double firstOperand, double secondOperand) {
+    return firstOperand - secondOperand;
+  }
 
-    /**
-     * Divide operation
-     */
-    public double div(double firstOperand, double secondOperand) {
-        return firstOperand / secondOperand;
-    }
+  /**
+   * Divide operation
+   */
+  public double div(double firstOperand, double secondOperand) {
+    if (secondOperand == 0) throw new IllegalArgumentException();
+    return firstOperand / secondOperand;
+  }
 
-    /**
-     * Multiply operation
-     */
-    public double mul(double firstOperand, double secondOperand) {
-        return firstOperand * secondOperand;
-    }
+  /**
+   * Multiply operation
+   */
+  public double mul(double firstOperand, double secondOperand) {
+    return firstOperand * secondOperand;
+  }
 }
--- Lesson3/Lab2/Challenge/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
+++ Lesson3/Lab2/Challenge/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
@@ -19,7 +19,9 @@ package com.example.android.SimpleCalc;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -37,6 +39,9 @@ public class CalculatorTest {
 
   private Calculator mCalculator;
 
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
   /**
    * Set up the environment for testing
    */
@@ -50,55 +55,55 @@ public class CalculatorTest {
    */
   @Test
   public void addTwoNumbers() {
-    double resultAdd = mCalculator.add(1d, 1d);
-    assertThat(resultAdd, is(equalTo(2d)));
+    double result = mCalculator.add(1d, 1d);
+    assertThat(result, is(equalTo(2d)));
   }
 
   @Test
   public void addTwoNumbersNegative() {
-    double resultAdd = mCalculator.add(-1d, 2d);
-    assertThat(resultAdd, is(equalTo(1d)));
+    double result = mCalculator.add(-1d, 2d);
+    assertThat(result, is(equalTo(1d)));
   }
 
   @Test
   public void addTwoNumbersFloats() {
-    double resultAdd = mCalculator.add(1.111f, 1.111d);
-    assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+    double result = mCalculator.add(1.111f, 1.111d);
+    assertThat(result, is(closeTo(2.222, 0.01)));
   }
 
   @Test
   public void subTwoNumbers() {
-    double resultAdd = mCalculator.sub(1d, 1d);
-    assertThat(resultAdd, is(equalTo(0d)));
+    double result = mCalculator.sub(1d, 1d);
+    assertThat(result, is(equalTo(0d)));
   }
 
   @Test
   public void subWorksWithNegativeResults () {
-    double resultAdd = mCalculator.sub(1d, 2d);
-    assertThat(resultAdd, is(equalTo(-1d)));
+    double result = mCalculator.sub(1d, 2d);
+    assertThat(result, is(equalTo(-1d)));
   }
 
   @Test
   public void mulTwoNumbers() {
-    double resultAdd = mCalculator.mul(2d, 3d);
-    assertThat(resultAdd, is(equalTo(6d)));
+    double result = mCalculator.mul(2d, 3d);
+    assertThat(result, is(equalTo(6d)));
   }
 
   @Test
   public void mulTwoNumbersZero() {
-    double resultAdd = mCalculator.mul(2d, 0d);
-    assertThat(resultAdd, is(equalTo(0d)));
+    double result = mCalculator.mul(2d, 0d);
+    assertThat(result, is(equalTo(0d)));
   }
 
   @Test
   public void divTwoNumbers() {
-    double resultAdd = mCalculator.div(4d, 2d);
-    assertThat(resultAdd, is(equalTo(2d)));
+    double result = mCalculator.div(4d, 2d);
+    assertThat(result, is(equalTo(2d)));
   }
 
   @Test
-  public void divTwoNumbersZero() {
-    double resultAdd = mCalculator.div(4d, 0d);
-    assertThat(resultAdd, is(equalTo(Double.POSITIVE_INFINITY)));
+  public void divByZeroThrows() {
+    thrown.expect(IllegalArgumentException.class);
+    mCalculator.div(4d, 0d);
   }
 }
