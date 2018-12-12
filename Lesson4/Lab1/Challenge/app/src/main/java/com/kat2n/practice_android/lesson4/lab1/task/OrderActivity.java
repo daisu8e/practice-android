13829cad2fe0025f69fc571c791fa69d6c36883d
kat2n <katsumata.coffee.150yen@gmail.com>
2018-12-12 15:33:34 -0800

    Lesson4 Lab1 Challenge

--- Lesson4/Lab1/Challenge/app/src/main/java/com/kat2n/practice_android/lesson4/lab1/task/OrderActivity.java
+++ Lesson4/Lab1/Challenge/app/src/main/java/com/kat2n/practice_android/lesson4/lab1/task/OrderActivity.java
@@ -1,7 +1,9 @@
 package com.kat2n.practice_android.lesson4.lab1.task;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class OrderActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_order);
+
+    Intent intent = getIntent();
+    String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+    TextView textView = findViewById(R.id.order_textview);
+    textView.setText(message);
   }
 }
