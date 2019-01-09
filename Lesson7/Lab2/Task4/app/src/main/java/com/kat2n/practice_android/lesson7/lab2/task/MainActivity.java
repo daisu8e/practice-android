1550b9270fb8e7741b44c0920bb39fe2193367f9
kat2n <katsumata.coffee.150yen@gmail.com>
2019-01-08 16:38:29 -0800

    Lesson7 Lab2 Task4

--- Lesson7/Lab2/Task4/app/src/main/java/com/kat2n/practice_android/lesson7/lab2/task/MainActivity.java
+++ Lesson7/Lab2/Task4/app/src/main/java/com/kat2n/practice_android/lesson7/lab2/task/MainActivity.java
@@ -3,6 +3,10 @@ package com.kat2n.practice_android.lesson7.lab2.task;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +14,11 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
   private EditText mBookInput;
   private TextView mTitleText;
@@ -24,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     mBookInput = findViewById(R.id.bookInput);
     mTitleText = findViewById(R.id.titleText);
     mAuthorText = findViewById(R.id.authorText);
+
+    if(getSupportLoaderManager().getLoader(0)!=null){
+      getSupportLoaderManager().initLoader(0,null,this);
+    }
   }
 
   public void searchBooks(View view) {
@@ -38,7 +50,9 @@ public class MainActivity extends AppCompatActivity {
       networkInfo = connMgr.getActiveNetworkInfo();
     }
     if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
-      new FetchBook(mTitleText, mAuthorText).execute(queryString);
+      Bundle queryBundle = new Bundle();
+      queryBundle.putString("queryString", queryString);
+      getSupportLoaderManager().restartLoader(0, queryBundle, this);
       mAuthorText.setText("");
       mTitleText.setText(R.string.loading);
     } else {
@@ -50,5 +64,56 @@ public class MainActivity extends AppCompatActivity {
         mTitleText.setText(R.string.no_network);
       }
     }
+  }
+
+  @NonNull
+  @Override
+  public Loader onCreateLoader(int id, @Nullable Bundle args) {
+    String queryString = "";
+
+    if (args != null) {
+      queryString = args.getString("queryString");
+    }
+
+    return new BookLoader(this, queryString);
+  }
+
+  @Override
+  public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+    try {
+      JSONObject jsonObject = new JSONObject(data);
+      JSONArray itemsArray = jsonObject.getJSONArray("items");
+      int i = 0;
+      String title = null;
+      String authors = null;
+      while (i < itemsArray.length() && (authors == null && title == null)) {
+        JSONObject book = itemsArray.getJSONObject(i);
+        JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+        try {
+          title = volumeInfo.getString("title");
+          authors = volumeInfo.getString("authors");
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        i++;
+      }
+      if (title != null && authors != null) {
+        mTitleText.setText(title);
+        mAuthorText.setText(authors);
+      } else {
+        mTitleText.setText(R.string.no_results);
+        mAuthorText.setText("");
+      }
+
+    } catch (JSONException e) {
+      mTitleText.setText(R.string.no_results);
+      mAuthorText.setText("");
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void onLoaderReset(@NonNull Loader<String> loader) {
+
   }
 }
